55bb8ba1f88c5bd53e3d2094eb9b99ab257be736
SSRayane <r.serir@spinalcom.com>
2026-02-23 12:47:46 +0100

    preload config format

--- preload_config.js
+++ preload_config.js
@@ -23,7 +23,11 @@
  */
 
 module.exports = {
-  runViewInfo: true,
+  // This file is is only used if RUN_STARTUP_TASK=1 in the .env file. It is used to preload data into the database when the server starts.
+
+  // array of server_id to preload view info 
+  runViewInfo: [
+  ],
   // array of server_id to preload static details + ticket list details
   runStaticDetails: [
   ],
